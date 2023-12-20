192cf14ad1cb90644c1281c1839b50440086f714
Ben Ross <mrbenross@gmail.com>
2023-12-20 12:51:45 -0600

    server estblished

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use('*', (req, res)=>{
+   res.status(404).json({
+    message: "You have an error"
+   })
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
