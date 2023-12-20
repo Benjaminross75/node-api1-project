820e8ce77b1112342818658490ab472ee4c67e38
Ben Ross <mrbenross@gmail.com>
2023-12-20 15:35:16 -0600

    update user function modified

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const server = express();
+server.use(express.json())
 const User = require('./users/model')
 
 server.get('/api/users', (req, res) => {
@@ -19,16 +20,73 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     const {id} = req.params
     User.findById(id)
+
     .then(user =>{
-       res.json(user)
+        if(!user){
+            res.status(404).json({
+                message: 'The user with the specified ID does not exist'
+            })
+        } else {
+            res.json(user)
+        }
+
     })
     .catch(err =>{
-      res.status(500).json({
-          message: 'User not found',
+      res.status(404).json({
+          message: 'The user with the specified ID does not exist',
           err: err.message,
       })
     })
   })
+
+  server.post('/api/users', (req, res) =>{
+    const user = req.body;
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    }else{
+        User.insert(user)
+        .then(createdUser =>{
+            res.status(201).json(createdUser)
+        })
+        .catch(err =>{
+            res.status(500).json({
+                message: "There was an error while saving the user to the database",
+                err: err.message
+            })
+        })
+    }
+})
+
+server.put('/api/users/:id', async (req, res)=>{
+ try{
+    const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser){
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    }else{
+        if(!req.body.name || !req.body.bio){
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else{
+         const updatedUser =  await User.update(req.params.id, req.body)
+         res.status(200).json(updatedUser)
+        }
+    }
+
+ } catch(err){
+    res.status(500).json({
+        message: "The user information could not be modified",
+        err: err.message
+    })
+ }
+
+
+
+})
 
 server.use('*', (req, res)=>{
    res.status(404).json({
