1e1595b77cd279bad51a8a5a7701d560a733be23
Ben Ross <mrbenross@gmail.com>
2023-12-20 13:16:36 -0600

    getting users enabled

--- api/server.js
+++ api/server.js
@@ -1,6 +1,20 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
 const server = express();
+const User = require('./users/model')
+
+server.get('/api/users', (req, res) => {
+  User.find()
+  .then(users =>{
+    res.send(users)
+  })
+  .catch(err =>{
+    res.status(500).json({
+        message: 'Error getting users',
+        err: err.message,
+    })
+  })
+})
 
 server.use('*', (req, res)=>{
    res.status(404).json({
